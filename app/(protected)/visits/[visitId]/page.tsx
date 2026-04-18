80b0df23c4bd2d72312e89ac943fc229f77b503a
Andrus Rähni <mugulane@gmail.com>
2026-04-18 16:47:46 +0300

    fix: mark visit detail page props as readonly

--- app/(protected)/visits/[visitId]/page.tsx
+++ app/(protected)/visits/[visitId]/page.tsx
@@ -2,9 +2,9 @@ import { VisitDetailPage } from "./visit-detail-page";
 
 export default async function Page({
   params,
-}: {
+}: Readonly<{
   params: Promise<{ visitId: string }>;
-}) {
+}>) {
   const { visitId } = await params;
 
   return <VisitDetailPage visitId={visitId} />;
