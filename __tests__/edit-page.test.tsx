9c7317d6b48c8588a05a13fc4936754bc6279e65
s1blik <ranno.mannikust@gmail.com>
2026-04-30 12:10:19 +0300

    run prettier --write

--- __tests__/edit-page.test.tsx
+++ __tests__/edit-page.test.tsx
@@ -1,10 +1,9 @@
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import React from "react";
 import EditKeycardPage from "@/app/(protected)/keys/[id]/edit/page";
 import { getKeycard, updateKeycard } from "@/lib/api/keycards";
 import { useCurrentUser } from "@/components/layout/current-user-provider";
-import { useRouter } from "next/navigation";
 
 // 1. MOCKID - Defineerime push funktsiooni väljaspool, et sellele ligi pääseda
 const mockPush = jest.fn();
